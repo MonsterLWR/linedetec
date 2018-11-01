9f774bb989db5a5b9bc2464aaf1733c9ca4e160a
MonsterLWR <31769298+MonsterLWR@users.noreply.github.com>
2018-11-01 15:10:42 +0800

    可输入大图片

--- detector.py
+++ detector.py
@@ -6,65 +6,98 @@ from lineDetector import LineDetector
 
 class Detector:
     def __init__(self):
-        self.lineDetector = LineDetector()
+        self.lineDetector = LineDetector(verbose=False)
 
-    def __detect_big_img(self, image, char_location, line_pos, dir=None, save=True):
-        for i in range(len(char_location)):
-            location = char_location[i]
-            digit = get_digit(image[location - 10:location + 20, -125:])
-            print(digit)
+    def __del_white(self, img):
+        """清除灰度化后图片的白边，以及大图片中间的白块"""
+        width = img.shape[1]
+        pos = width // 2
+        while img[0][pos] >= 245:
+            img = img[1:, :]
+        while img[-1][pos] >= 245:
+            img = img[:-1, :]
 
-            if i == len(char_location) - 1:
-                detected_img = image[location:, :]
-            else:
-                detected_img = image[location:char_location[i + 1], :]
+        # 删除大图片中间的白块
+        vertical_line = img[:, 0]
+        white_lines = np.where(vertical_line >= 240)
+        img = np.delete(img, white_lines, axis=0)
 
-            if save:
-                save_img(detected_img, dir, '{}.jpg'.format(digit))
+        return img
 
-            self.__detect_lines(detected_img, line_pos, digit, dir, save)
+    def __detect_big_img(self, image, char_location, line_pos, dir=None, save=False):
+        err_dir = os.path.join(dir, 'err_lines')
+        if not os.path.exists(err_dir):
+            os.mkdir(err_dir)
 
-    def __detect_lines(self, image, line_pos, img_name, dir=None, save=True):
+        lines_dir = None
+        if save:
+            lines_dir = os.path.join(dir, 'lines')
+            if not os.path.exists(lines_dir):
+                os.mkdir(lines_dir)
+
+        with open(os.path.join(err_dir, 'err.txt'), 'a') as f:
+            for i in range(len(char_location)):
+                location = char_location[i]
+                digit = get_digit(image[location - 10:location + 20, -125:])
+                print(digit)
+
+                if i == len(char_location) - 1:
+                    detected_img = image[location:, :]
+                else:
+                    detected_img = image[location:char_location[i + 1], :]
+
+                if save:
+                    save_img(detected_img, lines_dir, '{}.jpg'.format(digit))
+
+                self.__detect_lines(detected_img, line_pos, digit, lines_dir, err_dir, f, save)
+
+    def __detect_lines(self, image, line_pos, img_name, lines_dir=None, err_dir=None, err_txt=None, save=False):
         margin = (line_pos[2] - line_pos[0]) // 2
         for i in range(3):
             pos = line_pos[i]
             line_img = image[pos:pos + margin]
             line_name = '{}-{}.jpg'.format(img_name, i + 1)
             if save:
-                save_img(line_img, dir, line_name)
+                save_img(line_img, lines_dir, line_name)
 
             line_img = cv2.cvtColor(line_img, cv2.COLOR_GRAY2BGR)
             detected_line, err_flag = self.lineDetector.detect_error_line(line_img, line_name)
 
             if err_flag:
-                cv2.imshow(line_name, detected_line)
+                save_img(detected_line, err_dir, line_name)
+                print(line_name, file=err_txt)
+                # cv2.imshow(line_name, detected_line)
 
-    def detect(self, image, target_dir):
+    def detect(self, image_path, target_dir):
+        """检测image_path大图片中的异常，在target_dir中生成检测结果"""
+        image = read_gray_img(image_path)
+        image = self.__del_white(image)
+
         char_location = get_digit_position(image)
 
         # save_detected_img(origin_img, char_location, target_dir)
         line_pos = find_line_pos(image, char_location)
 
         # print(line_pos)
-        self.__detect_big_img(image, char_location, line_pos, target_dir)
+        self.__detect_big_img(image, char_location, line_pos, target_dir, save=True)
 
 
 if __name__ == '__main__':
     dir = 'digital_img'
-    target_dir = os.path.join(dir, 'split')
+    target_dir = os.path.join(dir, '20180602-001')
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
 
-    img_name = '20180530-002.bmp'
+    img_name = '20180602-001.bmp'
     file = os.path.join(dir, img_name)
-    origin_img = read_gray_img(file)
-    origin_img = del_white(origin_img)
+    # origin_img = read_gray_img(file)
+    # origin_img = del_white(origin_img)
     # _, img = cv2.threshold(origin_img, 5, 255, cv2.THRESH_BINARY)
 
-    origin_img = origin_img[-10000:, :]
+    # origin_img = origin_img[-10000:, :]
 
     detector = Detector()
-    detector.detect(origin_img, target_dir)
+    detector.detect(file, target_dir)
 
     # char_location = get_digit_position(origin_img)
 
@@ -74,4 +107,4 @@ if __name__ == '__main__':
     # print(line_pos)
     # detect_big_img(origin_img, char_location, line_pos, target_dir)
     # cv2.imshow('img', img)
-    cv2.waitKey()
+    # cv2.waitKey()
--- lineDetector.py
+++ lineDetector.py
@@ -10,7 +10,7 @@ class LineDetector:
         self.threshold = threshold
         self.verbose = verbose
         self.sample_count_per_hundred = sample_count_per_hundred
-        self.sec_size = round(sample_count_per_hundred / 2.5)
+        self.sec_size = round(sample_count_per_hundred / 2)
         self.show = show
 
     def __pre_process(self, img, threshold):
@@ -79,16 +79,16 @@ class LineDetector:
             lower_right = img[-1][-1]
         return img, offset
 
-    def __get_info(self, img, brightness, step):
-        """计算img中的，以step为间距的采样的列上线的宽度，线的边界数和线距离顶部的长度"""
+    def __get_info(self, img, gray, step):
+        """计算二值化后img中的，以step为间距的采样的列上线的宽度，线的边界数和线距离顶部的长度"""
         # widths:保存每个采样的列上，线的宽度
         # edge_counts:保存每个采样的列上，线的边界个数
         # first_edge_dis:保存每个采样的列上，第一个边界距离图像顶部的像素长度
         # vatys:保存每个采样的列上，线宽度的连续变化
         info = {'widths': [], 'edge_counts': [], 'first_edge_dis': [], 'varys': [],
                 'img_height': img.shape[0],
-                'img_width': img.shape[1],
-                'brightness': brightness}
+                'img_width': img.shape[1]}
+
         pos = 0
         while pos < info['img_width']:
             col = img[:, pos]
@@ -112,13 +112,15 @@ class LineDetector:
             pos += step
         info['varys'] = self.__get_varys(info['widths'], self.sec_size)
         info['slopes'] = self.__get_slopes(info['first_edge_dis'], self.sec_size)
+        info['brightness_max'] = self.__get_brightness(gray)
         if self.verbose:
             print('edge_counts:{}'.format(info['edge_counts']))
             print('first_edge_dis:{}'.format(info['first_edge_dis']))
             print('slopes:{}'.format(info['slopes']))
             print('widths:{}'.format(info['widths']))
             print('varys:{}'.format(info['varys']))
-            print('brightness:max:{},min:{}'.format(np.max(info['brightness']), np.min(info['brightness'])))
+            print('brightness_max:max:{},min:{}'.format(max(info['brightness_max']), min(info['brightness_max'])))
+            # print('brightness:{}'.format(info['brightness']))
         return info
 
     def __get_slopes(self, first_edge_dis, sec_size):
@@ -158,29 +160,8 @@ class LineDetector:
         # 两函数逻辑相同
         return self.__get_slopes(widths, sec_size)
 
-    # def __get_varys(self, widths):
-    #     """返回线宽度的连续变化值"""
-    #     varys = []
-    #     pre_wid = 0
-    #     for i, wid in enumerate(widths):
-    #         if i == 0:
-    #             pre_wid = wid
-    #             continue
-    #         vary = wid - pre_wid
-    #         if vary == 0:
-    #             varys.append(0)
-    #         elif len(varys) == 0:
-    #             varys.append(vary)
-    #         else:
-    #             varys.append(varys[-1] + vary)
-    #         pre_wid = wid
-    #     # print('varys:{}'.format(varys))
-    #     # print(varys.index(max(varys)))
-    #     abs_varys = [abs(vary) for vary in varys]
-    #     return abs_varys
-
-    def __draw_error_circle(self, origin_img, offset, info, step, var_limit, width_limit_max,
-                            width_limit_min, slope_limit, variation_limit):
+    def __find_error(self, origin_img, offset, info, step, var_limit, width_limit_max,
+                     width_limit_min, slope_limit, variation_limit, big_vary_count, brightness_limit):
         """在原图标识出出错的线，返回标识后的图片和是否进行过标识"""
         # 检测是否存在断线
         pre_width = 0
@@ -209,6 +190,19 @@ class LineDetector:
         if flag:
             return origin_img, True
 
+        if min(info['brightness_max']) <= self.threshold and max(info['widths']) > 0:
+            # 只断掉了一小截
+            indices = np.argmin(info['brightness_max'])
+            sample_index = indices // step
+            # 画红圈
+            cv2.circle(origin_img, (
+                offset['x'] + indices,
+                offset['y'] + info['first_edge_dis'][sample_index] + info['widths'][sample_index] // 2),
+                       30,
+                       (0, 0, 255),
+                       1)
+            return origin_img, True
+
         # 是否出现多个线的边界点
         flag = False
         for i, count in enumerate(info['edge_counts']):
@@ -221,6 +215,19 @@ class LineDetector:
                            (255, 0, 0),
                            1)
         if flag:
+            return origin_img, True
+
+        # 是否亮度异常
+        if max(info['brightness_max']) >= brightness_limit:
+            indices = np.argmax(info['brightness_max'])
+            sample_index = indices // step
+            # 画黄圈
+            cv2.circle(origin_img, (
+                offset['x'] + indices,
+                offset['y'] + info['first_edge_dis'][sample_index] + info['widths'][sample_index] // 2),
+                       30,
+                       (0, 255, 255),
+                       1)
             return origin_img, True
 
         # 是否粗细不合适
@@ -238,17 +245,6 @@ class LineDetector:
                           1)
             return origin_img, True
 
-        # 整体粗细变化是否太大
-        if var > var_limit:
-            # 画绿框
-            cv2.rectangle(origin_img,
-                          (offset['x'], offset['y'] + min(info['first_edge_dis']) - 5),
-                          (offset['x'] + info['img_width'],
-                           offset['y'] + max(info['first_edge_dis']) + max(info['widths']) + 5),
-                          (0, 255, 0),
-                          1)
-            return origin_img, True
-
         # 是否弯曲太厉害
         max_slope = max(info['slopes'])
         min_slope = min(info['slopes'])
@@ -262,8 +258,20 @@ class LineDetector:
                           1)
             return origin_img, True
 
-        # 局部粗细变化
+        # 整体粗细变化是否太大,包括方差过大和过多的局部粗细变化
         varys = info['varys']
+        if var > var_limit or np.where(np.abs(np.array(varys)) >= 2)[0].shape[0] >= big_vary_count:
+            # 画绿框
+            cv2.rectangle(origin_img,
+                          (offset['x'], offset['y'] + min(info['first_edge_dis']) - 5),
+                          (offset['x'] + info['img_width'],
+                           offset['y'] + max(info['first_edge_dis']) + max(info['widths']) + 5),
+                          (0, 255, 0),
+                          1)
+            return origin_img, True
+
+        # 局部粗细变化
+
         if max(varys) >= variation_limit:
             indexes = [i for i, vary in enumerate(varys) if vary == max(varys)]
             for index in indexes:
@@ -308,57 +316,65 @@ class LineDetector:
         return var, mean
 
     def __get_brightness(self, gray_img):
-        """将灰度图的亮度在竖直方向上做合"""
-        brightness = np.max(gray_img, axis=0)
-        return brightness
+        """将灰度图的亮度在竖直方向上做和,返回每一列上亮度的最大值"""
+        # brightness_min = np.min(np.where(gray_img >= self.threshold, gray_img, 255), axis=0)
+        # cv2.imshow('line', line_img)
+        brightness_max = np.max(gray_img, axis=0)
+        return brightness_max
 
-    def detect_error_line(self, origin_img, img_name, var_limit=2.0, width_limit_max=11.5, width_limit_min=6,
-                          slope_limit=5, variation_limit=3):
+    def detect_error_line(self, origin_img, img_name, var_limit=2.0, width_limit_max=11.5, width_limit_min=6.5,
+                          slope_limit=5, variation_limit=3, big_vary_count=4, brightness_limit=180):
         """origin_img为RGB图"""
         print('--------------------{}--------------------'.format(img_name))
         gray_img, pre_img = self.__pre_process(origin_img, self.threshold)
         del_img, offset = self.__del_white_margin(pre_img, self.margin)
+        # 为灰度图去掉白边
+        gray_img = gray_img[offset['y']:offset['y'] + del_img.shape[0], offset['x']:offset['x'] + del_img.shape[1]]
 
         if self.show:
-            cv2.imshow('del_img', del_img)
-        # cv2.waitKey()
+            cv2.imshow('del_{}'.format(img_name), del_img)
 
         sample_count = round(del_img.shape[1] / 100 * self.sample_count_per_hundred)
         step = del_img.shape[1] // sample_count
-        info = self.__get_info(del_img, self.__get_brightness(gray_img), step)
-        detect_img, err_flag = self.__draw_error_circle(origin_img, offset, info, step, var_limit, width_limit_max,
-                                                        width_limit_min, slope_limit, variation_limit)
+        info = self.__get_info(del_img, gray_img, step)
+        detect_img, err_flag = self.__find_error(origin_img, offset, info, step, var_limit, width_limit_max,
+                                                 width_limit_min, slope_limit, variation_limit, big_vary_count,
+                                                 brightness_limit)
         if self.verbose:
             print('offset:{}'.format(offset))
             print('raw_height:{},raw_width:{}'.format(origin_img.shape[0], origin_img.shape[1]))
             print('del_height:{},del_width:{}'.format(del_img.shape[0], del_img.shape[1]))
         if self.show:
-            cv2.imshow(img_name, detect_img)
+            cv2.imshow('detected_{}'.format(img_name), detect_img)
         return detect_img, err_flag
-        # cv2.waitKey()
 
 
 if __name__ == '__main__':
-    detector = LineDetector(verbose=True)
+    detector = LineDetector(verbose=True, show=True)
     dir = '20180530-002'
-    names = [
-        # '153-3.png',
-        # '154-3.png',
-        '341-3.jpg',
-        # '127-3.png',
-        # '150-2-n.png'
-    ]
-    file_names = os.listdir(dir)
-    detected_dir = os.path.join(dir, 'detected')
-    if not os.path.exists(detected_dir):
-        os.mkdir(detected_dir)
+    # names = [
+    #     # '153-3.png',
+    #     # '154-3.png',
+    #     '341-3.jpg',
+    #     # '127-3.png',
+    #     # '150-2-n.png'
+    # ]
+    # file_names = os.listdir(dir)
+    # detected_dir = os.path.join(dir, 'detected')
+    # if not os.path.exists(detected_dir):
+    #     os.mkdir(detected_dir)
+    #
+    # for file_name in file_names:
+    #     file_name_with_dir = os.path.join(dir, file_name)
+    #     if os.path.isfile(file_name_with_dir):
+    #         image = cv2.imread(file_name_with_dir)
+    #         detected_img, err = detector.detect_error_line(image, file_name)
+    #         if err:
+    #             target_file = os.path.join(detected_dir, file_name)
+    #             cv2.imwrite(target_file, detected_img)
 
-    for file_name in file_names:
-        file_name_with_dir = os.path.join(dir, file_name)
-        if os.path.isfile(file_name_with_dir):
-            image = cv2.imread(file_name_with_dir)
-            detected_img, err = detector.detect_error_line(image, file_name)
-            if err:
-                target_file = os.path.join(detected_dir, file_name)
-                cv2.imwrite(target_file, detected_img)
+    file_name = '2.png'
+    image = cv2.imread(file_name)
+    detected_img, err = detector.detect_error_line(image, file_name)
+    cv2.waitKey()
     # cv2.waitKey()
